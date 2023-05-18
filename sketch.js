b9662b3b364d6d3e038e7b1d8c2b8e0aa0357ccd
Matt Smallhouse <smallhou@bc.edu>
2023-05-18 18:46:12 -0400

    - Created and sized grid and squares dynamically in js rather than hard-coded in css, will be useful for getting user input on size of grid or # of squares - Added a button for user input, haven't set up prompt to do so yet

--- sketch.js
+++ sketch.js
@@ -1,17 +1,36 @@
+const GRID_WIDTH = 500;
+const GRID_HEIGHT = 500;
 const ROWS = 16;
 const COLUMNS = 16;
 
-const gridContainer = document.querySelector('.grid-container');
+function hover() {
+    this.style.backgroundColor = 'black';
+}
 
-for (let i=0; i<ROWS; i++) {
-    const rowContainer = document.createElement('div');
-    rowContainer.className = `row`;
-    gridContainer.appendChild(rowContainer);
+function setWindowSize() {
+    const gridContainer = document.querySelector('.grid-container');
+    gridContainer.style.width = `${GRID_WIDTH}`;
+    gridContainer.style.height = `${GRID_HEIGHT}`;
+}
 
-    for (let j=0; j<COLUMNS; j++) {
-        const newDiv = document.createElement('div');
-        newDiv.className = `col`;
-        newDiv.textContent = `div ${j}`;
-        rowContainer.appendChild(newDiv);
+function initGrid(rows, columns) {
+    for (let i=0; i<rows; i++) {
+        const rowContainer = document.createElement('div');
+        rowContainer.className = `row`;
+        rowContainer.style.height = `${GRID_WIDTH/rows}px`;
+        gridContainer.appendChild(rowContainer);
+
+        for (let j=0; j<columns; j++) {
+            const newDiv = document.createElement('div');
+            newDiv.className = `grid-square`;
+            newDiv.style.width = `${GRID_HEIGHT/columns}px`;
+            rowContainer.appendChild(newDiv);
+            newDiv.addEventListener('mouseover', hover);
+        }
     }
 }
+
+const gridContainer = document.querySelector('.grid-container');
+
+setWindowSize();
+initGrid(ROWS, COLUMNS);
